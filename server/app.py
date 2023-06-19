744d66e703ac3218a3497d5e61c347b2801bfca4
NitteshKhanna <guru.dikshit17945@gmail.com>
2023-06-19 21:35:57 +0530

    Backend completed

--- server/app.py
+++ server/app.py
@@ -4,10 +4,10 @@ import sqlite3
 app=Flask(__name__)
 con=sqlite3.connect("db.db")
 cur=con.cursor()
-cur.execute("CREATE TABLE students (name TEXT, rollno INTEGER, mail_id TEXT, address TEXT, no_of_arrears INTEGER, cgpa REAL, mobile_no TEXT);")
+cur.execute("CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, rollno INTEGER, mail_id TEXT, address TEXT, no_of_arrears INTEGER, cgpa REAL, mobile_no TEXT);")
 con.close()
 
-@app.route("data",methods=['GET'])
+@app.route("/data",methods=['GET'])
 def datag():
 
     con=sqlite3.connect("db.db")
@@ -17,16 +17,47 @@ def datag():
     con.close()
     return op
 
-@app.route("data",methods=['POST'])
+@app.route("/data",methods=['POST'])
 def datap():
-    json=request.form.get()
+
+    name=request.form.get("name")
+    rollno=request.form.get("rollno")
+    mail_id=request.form.get("mail_id")
+    add=request.form.get("add")
+    arrears=request.form.get("arrears")
+    cgpa=request.form.get("cgpa")
+    no=request.form.get("no")
+
+    q=(name,rollno,mail_id,add,arrears,cgpa,no)
 
     con=sqlite3.connect("db.db")
     cur=con.cursor()
-    cur.execute("INSERT INTO students (name, rollno, mail_id, address, no_of_arrears, cgpa, mobile_no) VALUES ('John Doe', 123456, 'johndoe@example.com', '123 Main St, City', 2, 3.5, '123-456-7890');")
+    cur.execute("INSERT INTO students (name, rollno, mail_id, address, no_of_arrears, cgpa, mobile_no) VALUES (?,?,?,?,?,?,?);",q)
     con.commit()
     con.close()
     return
+
+@app.route("/dataj",methods=['POST'])
+def dataj():
+
+    json=request.json 
+    name=json["name"]
+    rollno=json["rollno"]
+    mail_id=json["mail_id"]
+    add=json["add"]
+    arrears=json["arrears"]
+    cgpa=json["cgpa"]
+    no=json["no"]
+
+    q=(name,rollno,mail_id,add,arrears,cgpa,no)
+
+    con=sqlite3.connect("db.db")
+    cur=con.cursor()
+    cur.execute("INSERT INTO students (name, rollno, mail_id, address, no_of_arrears, cgpa, mobile_no) VALUES (?,?,?,?,?,?,?);",q)
+    con.commit()
+    con.close()
+    
+    return "Inserted"
 
 @app.get("/test")
 def test():
